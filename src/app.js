18deef9939db2cbab07ad518e973431c23a59d04
André Porto <andreporto@coderBR-MacBookPro.local>
2020-09-06 12:28:59 -0300

    Implementação do desafio de criar rotas GET, POST, PUT e DELETE para uma base em memória

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid, isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  let { title, url, techs} = request.body;
+  let id = uuid();
+  let repo = { id, title, url, techs, likes: 0 };
+  repositories.push(repo);
+  return response.status(201).json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  let { id } = request.params
+  let { title, url, techs } = request.body;
+  let index = repositories.findIndex( repo => repo.id == id);
+  if (index >= 0) {
+    let repo = repositories[index];
+    if (title != null) { repo.title = title}
+    if (url != null) { repo.url = url}
+    if (techs != null) { repo.techs = techs}
+    repositories[index] = repo;
+    return response.status(200).json(repo);
+  }
+  return response.status(400).json({message: "Bad Request"});
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  let { id } = request.params
+  let index = repositories.findIndex( repo => repo.id == id);
+  if (index >= 0) {
+    repositories.splice(index, 1);
+    return response.status(204).json();
+  }
+  return response.status(400).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  let { id } = request.params
+  let index = repositories.findIndex( repo => repo.id == id);
+  if (index >= 0) {
+    repo = repositories[index];
+    repo.likes++
+    return response.status(200).json(repo);
+  }
+  return response.status(400).json({message: "Bad Request"});
 });
 
 module.exports = app;
